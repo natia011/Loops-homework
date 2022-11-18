6e9d5d5028244e2d7270853e7994f2ef2b96e172
natia011 <116560823+natia011@users.noreply.github.com>
2022-11-18 12:20:17 +0400

    Add files via upload

---
+++ homework 3.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i,sum=0; 
+   cout << " ipove pirveli 10 naturaluri ricxvis sashualo: ";
+    cout << "---------------------------------------\n";
+   cout << "naturaluri ricxvebia: /n ";
+   for ( i=1; i <= 10; i++)
+   {
+       cout <<i<< "  ";
+       sum=sum+i;
+   }
+cout << sum<< endl;
+  
+}
+
+
+ 
